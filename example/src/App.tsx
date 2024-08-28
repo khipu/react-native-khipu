ea4315a7c0bf1ea5ea73c3e40b04115e36676cd9
Emilio Davis <emilio.davis@khipu.com>
2024-08-28 16:00:20 -0400

    docs: better example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import {
   type KhipuColors,
   type KhipuOptions,
@@ -10,32 +10,40 @@ import {
 
 export default function App() {
   const [result, setResult] = React.useState<KhipuResult | undefined>();
-
-  React.useEffect(() => {
-    startOperation({
-      operationId: 'pxz1smiyrmji',
-      options: {
-        title: 'KhipuReactNative',
-        titleImageUrl:
-          'https://s3.amazonaws.com/static.khipu.com/buttons/2024/200x75-black.png',
-        locale: 'es_CL',
-        theme: 'light',
-        skipExitPage: false,
-        showFooter: false,
-        showMerchantLogo: true,
-        showPaymentDetails: true,
-        colors: {
-          // lightBackground: '#0000ff',
-          // lightPrimary: '#ff00ff',
-          // lightTopBarContainer: '#ffffff',
-          // lightOnTopBarContainer: '#333333',
-        } as KhipuColors,
-      } as KhipuOptions,
-    }).then(setResult);
-  }, []);
+  const [id, setId] = React.useState<string | null>(null);
+  const start = function () {
+    if (id !== null) {
+      startOperation({
+        operationId: id,
+        options: {
+          title: 'KhipuReactNative',
+          titleImageUrl:
+            'https://s3.amazonaws.com/static.khipu.com/buttons/2024/200x75-black.png',
+          locale: 'es_CL',
+          theme: 'light',
+          skipExitPage: false,
+          showFooter: false,
+          showMerchantLogo: true,
+          showPaymentDetails: true,
+          colors: {
+            // lightBackground: '#0000ff',
+            // lightPrimary: '#ff00ff',
+            // lightTopBarContainer: '#ffffff',
+            // lightOnTopBarContainer: '#333333',
+          } as KhipuColors,
+        } as KhipuOptions,
+      }).then(setResult);
+    }
+  };
 
   return (
     <View style={styles.container}>
+      <TextInput
+        id={'id'}
+        onChangeText={setId}
+        placeholder={'PaymentId'}
+      ></TextInput>
+      <Button title={'Start'} onPress={start}></Button>
       <Text>OperationId: {result?.operationId}</Text>
       <Text>Result: {result?.result}</Text>
       <Text>ExitTitle: {result?.exitTitle}</Text>
